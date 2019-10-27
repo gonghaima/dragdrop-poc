2b99cc16de2bfb2f073866e11220398cbbecdb44
Steven <gonghaima@hotmail.com>
2019-10-27 17:39:48 +1100

    basic formatting on items

--- src/App.js
+++ src/App.js
@@ -6,22 +6,9 @@ import { data } from "./mock/data";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {data.map(d => <p>{d.description}</p>)}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="item-container">
+        {data.map((d, idx) => <div className="items" key={`item${idx}`}>{d.description}</div>)}
+      </div>
     </div>
   );
 }
