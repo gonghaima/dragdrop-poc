a4b16a67887908f08340bcc935c72867e48da856
Steven <gonghaima@hotmail.com>
2019-10-27 15:28:14 +1100

    setting up mock data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { data } from "./mock/data";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {data.map(d => <p>{d.description}</p>)}
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/mock/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+        id: 1,
+        price: "$87.68",
+        product_name: "Amitriptyline Hydrochloride",
+        description: "synergize efficient metrics",
+        product_image: "http://dummyimage.com/307x328.bmp/ff4444/ffffff"
+    },
+    {
+        id: 2,
+        price: "$58.53",
+        product_name: "Zicam",
+        description: "repurpose world-class metrics",
+        product_image: "http://dummyimage.com/345x342.jpg/dddddd/000000"
+    },
+    {
+        id: 3,
+        price: "$12.82",
+        product_name: "Home Care Fluoride",
+        description: "reinvent end-to-end relationships",
+        product_image: "http://dummyimage.com/318x336.png/5fa2dd/ffffff"
+    },
+    {
+        id: 4,
+        price: "$55.33",
+        product_name: "Amlodipine Besylate",
+        description: "optimize visionary channels",
+        product_image: "http://dummyimage.com/340x344.png/dddddd/000000"
+    },
+    {
+        id: 5,
+        price: "$30.22",
+        product_name: "Cyclobenzaprine Hydrochloride",
+        description: "reinvent back-end deliverables",
+        product_image: "http://dummyimage.com/324x341.bmp/dddddd/000000"
+    },
+    {
+        id: 6,
+        price: "$72.06",
+        product_name: "Buspirone HCl",
+        description: "exploit dot-com communities",
+        product_image: "http://dummyimage.com/303x315.png/cc0000/ffffff"
+    }];
+
+export { data }
