0722b294186e4abeece9caf21746f92c753e16f6
Steven <gonghaima@hotmail.com>
2019-10-28 19:04:47 +1100

    onDragOver added

--- src/dragComponent.js
+++ src/dragComponent.js
@@ -1,24 +1,32 @@
 
 import React from 'react';
-export const DragComponent = ({ data, sortData, letMeKnow }) => {
-    const dg = (event) => {
+export const DragComponent = ({ data, sortData, doSth }) => {
+    const drag = (event) => {
         console.log('dragging');
     }
-    const ds = (event) => {
-        debugger
+    const dragStart = (event) => {
+        console.log('drag start...');
         event.dataTransfer.setData("Text", event.target.id);
     }
+
+    const allowDrop = (event) => {
+        console.log('allowDrop triggered');
+        event.preventDefault();
+    }
+
     return (<div className="item-container">
-        {data.map((d, idx) =>
-            <div id={`item${idx}`}
-                className="items"
-                key={`item${idx}`}
-                draggable={true}
-                onDrag={(e) => dg(e)}
-                onDragStart={e => ds(e)}>
-                {d.description}
-            </div>
-        )
+        {
+            data.map((d, idx) =>
+                <div id={`item${idx}`}
+                    className="items"
+                    key={`item${idx}`}
+                    draggable={true}
+                    onDrag={(e) => drag(e)}
+                    onDragStart={e => dragStart(e)}
+                    onDragOver={event => allowDrop(event)}>
+                    {d.description}
+                </div>
+            )
         }
     </div >)
 }
